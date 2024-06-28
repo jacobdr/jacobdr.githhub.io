61475fa477bfbcc2a8ea1037d2a8cbcb909c004f
Jacob Roberts <jacobdr@gmail.com>
2024-06-28 11:57:09 -0400

    Initial commit

---
+++ src/consts.ts
@@ -0,0 +1,35 @@
+import type { Site, Metadata, Socials } from "@types";
+
+export const SITE: Site = {
+  NAME: "Astro Nano",
+  EMAIL: "markhorn.dev@gmail.com",
+  NUM_POSTS_ON_HOMEPAGE: 3,
+  NUM_WORKS_ON_HOMEPAGE: 2,
+  NUM_PROJECTS_ON_HOMEPAGE: 0,
+};
+
+export const HOME: Metadata = {
+  TITLE: "Home",
+  DESCRIPTION: "Astro Nano is a minimal and lightweight blog and portfolio.",
+};
+
+export const BLOG: Metadata = {
+  TITLE: "Blog",
+  DESCRIPTION: "A collection of articles on topics I am passionate about.",
+};
+
+export const WORK: Metadata = {
+  TITLE: "Work",
+  DESCRIPTION: "Where I have worked and what I have done.",
+};
+
+export const SOCIALS: Socials = [
+  {
+    NAME: "github",
+    HREF: "https://github.com/jacobdr",
+  },
+  {
+    NAME: "linkedin",
+    HREF: "https://www.linkedin.com/in/jacob-roberts-293a2228",
+  },
+];
